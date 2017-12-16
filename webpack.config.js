18936b56cd0c10d1cfe34f64f66427a8cb45bccf
moshe hanuka <mosheh77@gmail.com>
2017-12-16 23:20:35 +0200

    add webpack + webpack dev server

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require("path");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ["./app/main.js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "app.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "build"),
+    compress: true,
+    port: 8080
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
