00cee00008804bff125984bf5a2373d7fec90320
moshe hanuka <mosheh77@gmail.com>
2017-12-20 07:56:38 +0200

    added bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
         use: {
             loader: 'html-loader'
         }
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=8192'
       }
     ]
   },
